b20a9417dce25abdc789ad181c46de59f3c8e4fa
Eric Eckert <eeckert@seismo4.lbl.gov>
2019-12-04 12:27:08 -0800

    updated the 2d interpolate code to querry the interpolator instead of sampling it on a mesh, preparing for first live test

--- sw4_to_essi_2d_interpolate_On_mesh.py
+++ sw4_to_essi_2d_interpolate_On_mesh.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  My newley cleaned up and legible version of the interpolation codes
+#  get2dSliceNP.py
 #  
 #  Copyright 2019 Eric Eckert <dukeleto@spica>
 #  
@@ -29,233 +29,6 @@ import itertools
 import scipy
 from scipy import integrate
 from scipy import interpolate #used for its linear interpolation function
-
-
-class interpolateSW4():
-    def __init__(self,sw4essiout_filename,parameterFile):
-        #load the parameter file
-        self.pf = parameterFile
-        az = int(self.pf['azimuth'])
-        #check that the azimuth is valid
-        assert (int(az)==90 or int(az)==0)
-        if(az==90):
-            #I am using the sw4 x plane direction |
-            self.directivity = 'sw4_x'
-        else:
-            #I am using the sw4 y plane direction --
-            self.directivity = 'sw4_y'
-        #verify that a valid interpolation method has been selected
-        assert (self.pf["interpolate"]==None or self.pf["interpolate"]=="linear" or self.pf["interpolate"]=="cubic" or self.pf["interpolate"]=="quintic")
-        #now deal with the input parameter file informations
-        self.sw4essiout = h5py.File(sw4essiout_filename, 'r')
-        self.h  = self.sw4essiout['ESSI xyz grid spacing'][0]
-        self.x0 = self.sw4essiout['ESSI xyz origin'][0]
-        self.y0 = self.sw4essiout['ESSI xyz origin'][1]
-        self.z0 = self.sw4essiout['ESSI xyz origin'][2]
-        self.t0 = self.sw4essiout['time start'][0]
-        self.npts = self.sw4essiout['vel_0 ijk layout'].shape[0]
-        self.dt = self.sw4essiout['timestep'][0]
-        self.t1 = self.dt*(self.npts-1)
-        self.nt = self.sw4essiout['vel_0 ijk layout'].shape[0]
-        self.time = np.linspace(self.t0, self.t1, self.npts)
-        if(self.directivity == 'sw4_x'):
-            #load the input data
-            self.plane = int(int(self.pf['y0'])/self.h)
-        else:
-            #load the input data
-            self.plane = int(int(self.pf['x0'])/self.h)
-        #now save the plane for this orientation
-        if(self.directivity == 'sw4_x'):
-            #load the input data
-            #n_horrizontal = sw4essiout['vel_0 ijk layout'].shape[1]
-            #take all timesteps, and the plane as specified in the input file
-            #data_horrizontal etc. is all in ESSI COORDINATES!
-            self.data_horrizontal = self.sw4essiout['vel_0 ijk layout'][:,:,self.plane,:]
-            #set out of plane data to 0
-            self.data_out_of_plane = np.zeros(shape=self.sw4essiout['vel_1 ijk layout'][:,:,self.plane,:].shape)
-            self.data_vertical = -1.0*self.sw4essiout['vel_2 ijk layout'][:,:,self.plane,:]
-        else:
-            #n_horrizontal = sw4essiout['vel_1 ijk layout'].shape[0]
-            self.data_horrizontal = self.sw4essiout['vel_1 ijk layout'][:,self.plane,:,:]
-            #set out of plane data to 0
-            self.data_out_of_plane = np.zeros(shape=self.sw4essiout['vel_0 ijk layout'][:,self.plane,:,:].shape)
-            self.data_vertical = -1.0*self.sw4essiout['vel_2 ijk layout'][:,self.plane,:,:]
-    def interpolate(self):
-        #save the horrizontal and veritical interpolation formulas in a list where index corresponds to time step
-        self.hInterpolator = []
-        self.vInterpolator = []
-        #generate the coordinate basis so that the interpolator knows where this information is located spacially
-        hMax,vMax = (self.data_horrizontal.shape[1])*self.h,(self.data_horrizontal.shape[2])*self.h
-        xGrid,zGrid = np.arange(0,hMax,self.h),np.arange(0,vMax,self.h)
-        #create interpolateor for the horrizontal data using this method and deal with every time step
-        for t in (range(self.data_horrizontal.shape[0])):
-            self.hInterpolator.append(interpolate.interp2d(xGrid, zGrid, self.data_horrizontal[t,:,:], kind=self.pf["interpolate"]))
-        #now do the same for the veritcal data--The geometery of the problem is the same so I just have to change to data vertical
-        for t in (range(self.data_vertical.shape[0])):
-            self.vInterpolator.append(interpolate.interp2d(xGrid, zGrid, self.data_vertical[t,:,:], kind=self.pf["interpolate"]))
-
-    def evalAtPoint(self,sw4x,sw4z):
-        #creates a time series for the specified point using the specified interpolation function
-        #this is then returned to the caller
-        shape = (self.data_horrizontal.shape[0])
-        horrizontalPlane,verticalPlane = np.zeros(shape=shape,dtype=np.float32),np.zeros(shape=shape,dtype=np.float32)
-        #loop over everytime step and save the interpolated data for that time step
-        for t in range(shape):
-            horrizontalPlane[t] = self.hInterpolator[t](sw4x,sw4z)
-            verticalPlane[t] = self.vInterpolator[t](sw4x,sw4z)
-        return horrizontalPlane,verticalPlane
-    def closeContainer(self):
-        self.sw4essiout.close()
-
-class geometeryFile():
-    def __init__(self,drm_filename,parameterFile,timeSeries,nt,az):
-        self.drm_file = h5py.File(drm_filename,"r+")
-        #self.sw4h = sw4h
-        self.pf = parameterFile
-        self.x0,self.y0 = float(parameterFile["x0"]),float(parameterFile["y0"])
-        self.essiXstart,self.essiYstart,self.essiZstart = float(parameterFile["essiXstart"]),float(parameterFile["essiYstart"]),float(parameterFile["essiZstart"])
-        #direction
-        assert (int(az)==90 or int(az)==0)
-        if(az==90):
-            #I am using the sw4 x plane direction |
-            self.directivity = 'sw4_x'
-        elif(az==0):
-            #I am using the sw4 y plane direction --
-            self.directivity = 'sw4_y'
-        self.coordinates = self.drm_file['Coordinates'][:]
-        self.n_coord = int(self.coordinates.shape[0] / 3)
-        #convert the coordinates to a list
-        self.coordinates = list(self.coordinates.reshape(self.coordinates.shape[0]//3,-1,3))
-        #create data sets for outputs (as necessary)
-        if not "Time" in self.drm_file.keys():
-            self.Time_dset          = self.drm_file.create_dataset("Time", data=timeSeries)
-            self.Accelerations_dset = self.drm_file.create_dataset("Accelerations", (self.n_coord*3, nt))
-            self.Displacements_dset = self.drm_file.create_dataset("Displacements", (self.n_coord*3, nt))
-        else:
-            del self.drm_file['Time']
-            self.Time_dset = self.drm_file.create_dataset("Time", data=timeSeries)
-            del self.drm_file['Accelerations']
-            self.Accelerations_dset = self.drm_file.create_dataset("Accelerations", (self.n_coord*3, nt))
-            del self.drm_file['Displacements']
-            self.Displacements_dset = self.drm_file.create_dataset("Displacements", (self.n_coord*3, nt))
-        
-    def generateSW4coordinates(self):
-        #convert ess coordinates to there corresponding sw4 coordinates
-        self.convertedCorrdinates = []
-        for coordinates in self.coordinates:
-            #check the orientation
-            if(self.directivity=='sw4_x'):
-                x,z = coordinates[0][1]-self.essiYstart+self.x0,self.essiZstart-coordinates[0][2]
-                self.convertedCorrdinates.append(np.array([x,z]))
-            
-            elif(self.directivity=='sw4_y'):
-                x,z = coordinates[0][0]-self.essiXstart+self.y0, self.essiZstart-coordinates[0][2]
-                self.convertedCorrdinates.append(np.array([x,z]))
-
-    def saveMotions(self,interpolator):
-        #evaluates the motions using the sw4 interpolator
-        for i in range(len(self.convertedCorrdinates)):
-            #save the time series for this point saving horrizontal and vertical velocities
-            v_h,v_v = interpolator.evalAtPoint(self.convertedCorrdinates[i][0],self.convertedCorrdinates[i][1])
-            #save the acceleration
-            a_h = np.gradient(v_h[:],interpolator.dt,axis=0)
-            a_v = np.gradient(v_v [:],interpolator.dt,axis=0)
-            #save the displacement
-            disp_h = scipy.integrate.cumtrapz(y=v_h[:],dx=interpolator.dt,initial=0,axis=0)
-            disp_v = scipy.integrate.cumtrapz(y=v_v[:],dx=interpolator.dt,initial=0,axis=0)
-            #assign to ESSI data set based on the current orientation
-            if(self.directivity=='sw4_x')
-                self.Accelerations_dset[i*3,:] = 0.0
-                self.Accelerations_dset[i*3+1,:] = a_h 
-                self.Accelerations_dset[i*3+2,:] = a_v
-                
-                self.Displacements_dset[i*3,:] = 0.0
-                self.Displacements_dset[i*3+1,:] = disp_h 
-                self.Displacements_dset[i*3+2,:] = disp_v
-            else:
-                self.Accelerations_dset[i*3,:] = a_h 
-                self.Accelerations_dset[i*3+1,:] = 0.0
-                self.Accelerations_dset[i*3+2,:] = a_v
-                
-                self.Displacements_dset[i*3,:] = disp_h 
-                self.Displacements_dset[i*3+1,:] = 0.0
-                self.Displacements_dset[i*3+2,:] = disp_v
-                            
-            #stop
-            
-            break
-    def closeContainer(self):
-        self.drm_file.close()
-
-"""
-Check that the integration and differentiation look reasonalbe for a point
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-
-fig, ax = plt.subplots()
-ax.plot(time, disp_v)
-fig.savefig("disp_plot_test.png")
-
-fig, ax = plt.subplots()
-ax.plot(time, a_v)
-fig.savefig("acc_plot_test.png")
-"""
-def dframeToDict(dFrame):
-    #takes the input dataframe, unfucks it, and returns a dictionary
-    dFrame = list(dFrame.iterrows())
-    return {i[1].to_list()[0] : i[1].to_list()[1] for i in dFrame}
-
-pfName = "get2dSlice.csv"
-geomFileName = "DRM_input_Model24.hdf5"
-sw4MotionsFileName = "Location17faultParallel.cycle=00000.essi"
-#load up the parameter file
-parameterFile = dframeToDict(pd.read_csv(pfName))
-#load up the sw4 motions
-sw4Data = interpolateSW4(sw4MotionsFileName,parameterFile)
-#prepare the interpolator
-sw4Data.interpolate()
-#create the time series for essi
-time = np.linspace(sw4Data.t0, sw4Data.t1, sw4Data.npts)
-#load up the geometery file
-essiGeom = geometeryFile(geomFileName,parameterFile,time,sw4Data.npts,int(parameterFile["azimuth"]))
-print('# of coordinates: ', essiGeom.n_coord)
-#convert all essi coordinates to sw4 coordinates
-essiGeom.generateSW4coordinates()
-#go through the geometery file, evaluate every point and save the resulting time series
-essiGeom.saveMotions(sw4Data)
-#close both containers
-essiGeom.closeContainer()
-sw4Data.closeContainer()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-All of this stuff pertains to the old version
 
 def dframeToDict(dFrame):
     #takes the input dataframe, unfucks it, and returns a dictionary
@@ -323,7 +96,6 @@ else:
     data_vertical = -1.0*sw4essiout['vel_2 ijk layout'][:,plane,:,:]
 
 """
-"""
 #check the spacing requested and the input spacing, if they disagree interpolate the inpute data!
 if(int(parameterFile["target_h"]) != int(sw4essiout["ESSI xyz grid spacing"][0])):
     #interpolate with the specified algorithm
@@ -331,7 +103,6 @@ if(int(parameterFile["target_h"]) != int(sw4essiout["ESSI xyz grid spacing"][0])
      + " to " + str(parameterFile["target_h"]) )
     
     #
-"""
 """
 #load up the geometery file
 drm_file = h5py.File(drm_filename,"a")
@@ -387,9 +158,7 @@ if(parameterFile["essi_units"]=="feet"):
         interpolatedPlaneVertical[t,:,:] = interpolator(newXGrid,newZGrid)
     #test it by plotting what the interpolated plane looks like
     
-"""
-    
-"""
+    """
     #test code to plot the above and verify that it functions correctly:
     import matplotlib.pyplot as plt
     #identify the time step with the maxium average value and plot there
@@ -424,8 +193,8 @@ if(parameterFile["essi_units"]=="feet"):
     #save it
     plt.savefig("origionalVsInterpolated.png",dpi=400)
     plt.close()
-"""
-"""
+    """
+    
     #assign data_horrizontal and data_vertical to the interpolated data
     data_horrizontal,data_vertical = interpolatedPlaneHorrizontal,interpolatedPlaneVertical
     
@@ -515,8 +284,6 @@ data_horrizontal_acc = np.gradient(data_horrizontal[:,:,:],dt,axis=0)
 data_vertical_acc = np.gradient(data_vertical[:,:,:],dt,axis=0)
 
 """
-
-"""
 Check that the integration and differentiation look reasonalbe for a point
 import matplotlib
 import matplotlib.pyplot as plt
@@ -529,7 +296,6 @@ fig.savefig("disp_plot_test.png")
 fig, ax = plt.subplots()
 ax.plot(time, data_horrizontal_acc[:,0,0])
 fig.savefig("acc_plot_test.png")
-"""
 """
 #save each coordinate to the container
 coordinateList = coordinates
@@ -579,7 +345,6 @@ for coordinate in range(len(coordinateList)):
 sw4essiout.close()
 drm_file.close()
 
-"""
 
 """
 def main(args):
